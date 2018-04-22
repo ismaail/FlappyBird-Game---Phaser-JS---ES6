1c6cd48dddfc0ed726c6a27869c415200a71d2bf
ismaail <contact@ismaail.com>
2018-04-22 01:31:33 +0100

    Add GameOver State

---
+++ src/states/GameOver.js
@@ -0,0 +1,17 @@
+import Phaser from 'phaser';
+
+/**
+ * Class GameOver
+ */
+export default class extends Phaser.State {
+    /**
+     * State Create
+     */
+    create() {
+        let image = this.game.add.image(this.game.width/2, this.game.height/2, 'gameover');
+
+        image.anchor.setTo(0.5, 0.5);
+
+        this.game.add.existing(image);
+    }
+}
